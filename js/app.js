d012649c362a00e350b3b239e9f65a3ae276912e
NicolaFlamel <taurus93@ukr.net>
2014-06-27 17:30:58 +0300

    Fix project

--- js/app.js
+++ js/app.js
@@ -1,157 +1,136 @@
-var x, y, i;
-var startDraggingX, stopDraggingX, directionX;
-var startDraggingY, stopDraggingY, directionY;
+;(function ($) {
 
-var slides = $('#slider .first-line').children().length;
-sliderWidth = $('#slider').width(),
-    sliderHeight = $('#slider').height(),
-    heightMin = 40,
-    stringCounter = $("#slider ul > div").length,
-    heightMax = -((stringCounter - 1) * sliderHeight + 40),
-    widthMin = 40,
-    widthMax = -((slides - 1) * sliderWidth + 40);
+    var carousel = new Carousel("info/carousel.json");
 
-var rightNavigation = [],
-    bottomNavigation = [];
+    var x, y,
+        startDraggingX,
+        startDraggingY,
+        slides,
+        sliderWidth,
+        sliderHeight,
+        heightMin,
+        heightMax,
+        widthMin,
+        widthMax;
 
-var uiStartPositionLeft, uiFinishPositionLeft;
-var uiStartPositionTop, uiFinishPositionTop;
+    var uiStartPositionLeft,
+        uiFinishPositionLeft;
+    var uiStartPositionTop,
+        uiFinishPositionTop;
 
-init(); //Slider initialization
+    var stringCounter;
+    var counter;
 
-$("#slider ul").draggable({
-    start: function (event, ui) {
-        x = event.pageX;
-        y = event.pageY;
-        startDraggingX = ui.position.left; //Start of draging
-        startDraggingY = ui.position.top;
-        uiStartPositionLeft = (ui.position.left).roundTo(sliderWidth);
-        uiStartPositionTop = (ui.position.top).roundTo(sliderHeight);
-    },
-    drag: function (event, ui) {
-        if (x && y) {
-            var axis = Math.abs(event.pageX - x) > Math.abs(event.pageY - y) ? 'x' : 'y'; //check dragging axis
-            $(this).draggable('option', 'axis', axis);
-            x = y = null;
+    function Carousel(infoFile) {
+        $.getJSON(infoFile, function (data) {
+            for (counter in data) {
+                addPage(data[counter]);
+                stringCounter = $(" #slider  ul ").length;
+                slides = $(" #slider " + "." + data[counter].className + " li ").length,
+                sliderWidth = $('#slider').width();
+                sliderHeight = $('#slider').height();
+                heightMin = 40;
+                heightMax = -((stringCounter - 1) * sliderHeight + 40);
+                widthMin = 40;
+                widthMax = -((slides - 1) * sliderWidth + 40);
+
+                $("#slider ul").draggable({
+                    axis: "x",
+                    start: function (event, ui) {
+                        x = event.pageX;
+                        y = event.pageY;
+                        startDraggingX = ui.position.left; //Start of draging
+                        startDraggingY = ui.position.top;
+                        uiStartPositionLeft = roundTo(ui.position.left, sliderWidth);
+                    },
+                    drag: function (event, ui) {
+                        if (x && y) {
+                            var axis = Math.abs(event.pageX - x) > Math.abs(event.pageY - y) ? 'x' : 'y'; //check dragging axis
+                            if (axis == "x") {
+                                $(this).draggable({axis: "x"});
+                                $("#slider").draggable({disabled: true});
+                                if (ui.position.left > widthMin) {
+                                    ui.position.left = widthMin;
+                                } else if (ui.position.left < widthMax) {
+                                    ui.position.left = widthMax;
+                                }
+                            } else if (axis == "y") {
+                                $(this).draggable({disabled: true});
+                                $("#slider").draggable({
+                                    axis: "y",
+                                    start: function (event, ui) {
+                                        uiStartPositionTop = roundTo(ui.position.top, sliderHeight);
+                                    },
+                                    drag: function (event, ui) {
+                                        $(this).draggable({axis: "y"});
+                                        if (ui.position.top > heightMin) {
+                                            ui.position.top = heightMin;
+                                        }
+                                        else if (ui.position.top < heightMax) {
+                                            ui.position.top = heightMax;
+                                        }
+                                    },
+                                    stop: function (event, ui) {
+                                        $(this).animate({'top': roundTo(ui.position.top, sliderHeight)}, "fast", function () {
+                                            uiFinishPositionTop = roundTo(ui.position.top, sliderHeight);
+                                            getPages(uiStartPositionTop, uiFinishPositionTop, "marker-right");
+                                        });
+                                        $("#slider ul").draggable({disabled: false});
+                                    }
+                                });
+                            }
+                        }
+                    },
+                    stop: function (event, ui) {
+                        $(this).animate({'left': roundTo(ui.position.left, sliderWidth)}, "fast", function () {
+                            uiFinishPositionLeft = roundTo(ui.position.left, sliderWidth);
+                            getPages(uiStartPositionLeft, uiFinishPositionLeft, "marker-bottom");
+                        });
+                        $("#slider").draggable({disabled: false});
+                    }
+                });
+            }
+            getPage(stringCounter);
+        });
+
+    }
+
+    function roundTo(object, size) {
+        size = size || 10;
+        return Math.round(object * (1 / (size))) * size;
+    }
+
+    function addPage(data) {
+        $("#slider").append("<ul class= " + data.className + ">" + data.content + "</ul>");
+    }
+
+    function getPages(start, finish, marker) {
+        if (start > finish) {
+            $("." + marker).removeClass(marker).next().addClass(marker);
+        } else if (start < finish) {
+            $("." + marker).removeClass(marker).prev().addClass(marker);
         }
+    }
 
-        //Border check of slider
-        if (ui.position.left > widthMin) {
-            ui.position.left = widthMin;
-
-        } else if (ui.position.left < widthMax) {
-            ui.position.left = widthMax;
+    function getPage(width) {
+        var i
+        var rightNavigation = []
+        for (i = 0; i < width; i++) {
+            rightNavigation.push("<li></li>");
         }
+        $(".right-navigation ul").append(rightNavigation);
+        $(".right-navigation li:eq(0)").addClass("marker-right");
+    }
 
-        if (ui.position.top > heightMin) {
-            ui.position.top = heightMin;
-        }
-        else if (ui.position.top < heightMax) {
-            ui.position.top = heightMax;
 
-        }
-
-        stopDraggingX = ui.position.left; //Finish of draging
-        stopDraggingY = ui.position.top; //Finish of draging
-
-        directionX = ((startDraggingX < stopDraggingX) ? 'right' : 'left'); // If startDragging is lower than stopDragging than direction if right
-        directionY = ((startDraggingY < stopDraggingY) ? 'down' : 'up');
-        insert(directionX, directionY);
-
-    },
-    stop: function (event, ui) {
-        //move the page if it is not equal to the border
-        x = y = null;
-
-        $(this).animate({'left': (ui.position.left).roundTo(sliderWidth)}, "fast", function () {
-            uiFinishPositionLeft = (ui.position.left).roundTo(sliderWidth);
-            getPages(uiStartPositionLeft, uiFinishPositionLeft, "marker-bottom");
+    $(window).resize(function () {
+        $('#wrapper').css({
+            position: 'absolute',
+            left: ($(document).width() - $('#wrapper').outerWidth()) / 2,
+            top: ($(document).height() - $('#wrapper').outerHeight()) / 2
         });
-        $(this).animate({'top': (ui.position.top).roundTo(sliderHeight)}, "fast", function () {
-            uiFinishPositionTop = (ui.position.top).roundTo(sliderHeight);
-            getPages(uiStartPositionTop, uiFinishPositionTop, "marker-right");
-        });
-    }
-});
+    });
+    $(window).resize();
 
-function insert(directionX, directionY) {
-    var elementUp = getPage().parent().prev().children().eq(getPage().prevAll().length),  //take the element above visible page
-        elementDown = getPage().parent().next().children().eq(getPage().prevAll().length),//take the element under visible page
-        elementNext = getPage().next(), //take next element of visible element
-        elementPrev = getPage().prev(), //take prev element of visible element
-        elementNextAll = getPage().nextAll(),
-        elementPrevAll = getPage().prevAll(),
-        elementParent = getPage().parent();
-
-    if (directionX == "right") {
-        addPage(elementPrev);
-        removePage(elementNextAll, elementParent);
-    } else if (directionX == "left") {
-        addPage(elementNext);
-        removePage(elementPrevAll, elementParent);
-    }
-
-    if (directionY == "down") {
-        addPage(elementUp);
-    } else if (directionY == "up") {
-        addPage(elementDown);
-    }
-}
-
-function addPage(currentPage) {
-    if ($(currentPage).length) {
-        currentPage.css({
-            backgroundImage: "url(images/" + currentPage.attr("id") + ".jpg)"
-        });
-    }
-}
-
-function removePage(removeCurrentPage, removeCurrentParent) {
-    if ($(removeCurrentPage).length) {
-        removeCurrentPage.css({
-            backgroundImage: "none"
-        });
-        removeCurrentParent.next().children().css({
-            backgroundImage: "none"
-        });
-        removeCurrentParent.prev().children().css({
-            backgroundImage: "none"
-        });
-    }
-
-}
-
-function getPage(e) {
-    e = e || window.event;
-    var event = window.event.srcElement || e.target;
-    return $(event);
-}
-
-function getPages(start, finish, marker) {
-    if (start > finish) {
-        $("." + marker).removeClass(marker).next().addClass(marker);
-    } else if (start < finish) {
-        $("." + marker).removeClass(marker).prev().addClass(marker);
-    }
-}
-
-Number.prototype.roundTo = function (nTo) {
-    nTo = nTo || 10;
-    return Math.round(this * (1 / nTo)) * nTo;
-}
-
-function init() {
-    for (i = 0; i < stringCounter; i++) {
-        rightNavigation.push("<li></li>")
-    }
-    $(".right-navigation").append(rightNavigation);
-
-    for (i = 0; i < slides; i++) {
-        bottomNavigation.push("<li ></li>")
-    }
-    $(".bottom-navigation").append(bottomNavigation);
-    
-    $(".bottom-navigation li:eq(0)").addClass("marker-bottom");
-    $(".right-navigation li:eq(0)").addClass("marker-right");
-}
+})(jQuery);
 
